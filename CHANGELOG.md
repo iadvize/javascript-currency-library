623469317b8ad78c3527ac593855f889b628f326
YvesPasteur <pro@ypasteur.fr>
2016-10-07 09:52:07 +0200

    doc(misc): Add some comments and defines the new package version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## [0.1.0](https://github.com/iadvize/javascript-currency-library/tree/0.1.0) (2016-10-07)
+
+**Implemented feature:**
+
+- Allow to list all the supported currencies
+- Change the text of the thrown errors to use the word 'supported' instead of 'defined'
+
 ## [0.0.1](https://github.com/iadvize/javascript-currency-library/tree/0.0.1) (2016-08-08)
 
 **Implemented feature:**
--- index.js
+++ index.js
@@ -12,6 +12,15 @@
     };
 
     return {
+      /**
+       * Get an currency object if it is supported
+       *
+       * @params {string} Currency code (ISO-4217)
+       *
+       * @returns {object} Object with two functions to get all the countries in which the currency is used
+       *                   and to know if it is used in a specific country
+       * @throws Error Throws an error if the currency is not supported
+      */
       get: function(currency) {
         if (currencies[currency]) {
           return {
@@ -25,6 +34,14 @@
         }
         throw new Error('The currency ' + currency + ' is not supported');
       },
+      /**
+       * find the currency used in a specific country
+       *
+       * @params {string} country Country code (ISO 3166-1 alpha-2)
+       *
+       * @returns {string} Currency code (ISO-4217)
+       * @throws Error Throws an error if the country is not supported
+      */
       findByCountry: function(country) {
         for (var currency in currencies) {
           if (currencies.hasOwnProperty(currency) && isAvailableIn(country, currency)) {
@@ -33,6 +50,11 @@
         }
         throw new Error('The country ' + country + ' is not supported');
       },
+      /**
+       * List all the supported currencies
+       *
+       * @returns {Array} List of currency codes (ISO-4217)
+      */
       getSupportedCurrencies: function() {
         return Object.getOwnPropertyNames(currencies);
       }
